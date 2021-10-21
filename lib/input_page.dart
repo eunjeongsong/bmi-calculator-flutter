ae5132029c4f0f08e487bd140a2bb751f69563e6
EUNJEONGSONG <hellogine12@gmail.com>
2021-10-21 22:59:07 +0900

    extract card widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -14,8 +15,46 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ResusableCard(
+                    colour: const Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ResusableCard(
+                    colour: const Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ResusableCard(
+              colour: const Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ResusableCard(
+                    colour: const Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ResusableCard(
+                    colour: const Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Theme(
         data: ThemeData(
@@ -27,6 +66,23 @@ class _InputPageState extends State<InputPage> {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
+      ),
+    );
+  }
+}
+
+class ResusableCard extends StatelessWidget {
+  ResusableCard({Key? key, required this.colour}) : super(key: key);
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
